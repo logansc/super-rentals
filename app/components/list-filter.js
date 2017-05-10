03408a3ba94f8343aea82191d4e5ed79825fd7dc
Scovil <logansc21@gmail.com>
2017-05-09 19:55:30 -0600

    unresolved error

---
+++ app/components/list-filter.js
@@ -0,0 +1,32 @@
+import Ember from 'ember';
+
+// As you would expect, this is simply inheriting from Ember.Component
+// The export default is just exporting this entire child object
+export default Ember.Component.extend({
+    classNames:['list-filter'],
+    value: '',
+
+    // In a component's lifecycle, init is the first method called.  See here: https://guides.emberjs.com/v2.6.0/components/the-component-lifecycle/
+    init() { 
+        // this._super calls the parent constructor, as you would expect.  I don't think that it's
+        // actually a javascript keyword, I think that ember just implemented the _super method themselves
+        // 'super' is a keyword, though.
+        this._super(...arguments);  
+        // Calling a method with two sets of parenthesis means that the first function returns a
+        // function, which is then immediately called with the second parenth's parameter(s).  
+        // The 'then' is a javascript promise--once filter returns, (I assume that 'results' is the 
+        // return value) then the code in the parenthesis executes.
+        // The code in the parenths is a new javascript shorthand that means results is used as a parameter
+        // for this.set (along with 'results' string).  It's the same as doing:
+        // function (results) {return this.set('results', results);}
+        this.get('filter')('').then((results) => this.set('results', results));
+    },
+
+    actions: {
+        handleFilterEntry() {
+            let filterInputValue = this.get('value');
+            let filterAction = this.get('filter');
+            filterAction(filterInputValue).then((filterResults) => this.set('results', filterResults));
+        }
+    }
+});
